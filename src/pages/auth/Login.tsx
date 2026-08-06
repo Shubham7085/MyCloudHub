1c12a8202d486ce6a5025b21bd4ffafa80d5e655
Shubham7085 <shubhamnagvanshi5@gmail.com>
2026-08-06 11:39:01 +0530

    Change Login component to named export

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Login() {
+export function Login() {
   return (
     <div style={{ 
       display: 'flex', 
