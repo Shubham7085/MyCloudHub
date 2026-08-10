39752bdc13b25e2f82ea5ad73c9a815ca9759f40
Shubham7085 <shubhamnagvanshi5@gmail.com>
2026-08-10 13:46:59 +0530

    Added Android App files

---
+++ android/app/src/main/java/com/mycloudhub/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mycloudhub.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mycloudhub.app',
+  appName: 'MyCloudHub',
+  webDir: 'dist'
+};
+
+export default config;
