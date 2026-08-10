c1daf27898a57ec3e18f81461f89aa38014dd801
Shubham7085 <shubhamnagvanshi5@gmail.com>
2026-08-10 20:34:07 +0530

    Configure CapacitorHttp plugin in capacitor.config.ts
    Added CapacitorHttp plugin configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mycloudhub.app',
   appName: 'MyCloudHub',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
+
